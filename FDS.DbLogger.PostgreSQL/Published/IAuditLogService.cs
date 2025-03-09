ef6d05587864b9cc689ac820aec0ed65203d353b
unknown <flavio.santos.ti@outlook.com>
2025-03-09 13:31:00 -0300

    ✨ feat: Add LogValidationErroAsync() for validation erros.

--- FDS.DbLogger.PostgreSQL/Published/IAuditLogService.cs
+++ FDS.DbLogger.PostgreSQL/Published/IAuditLogService.cs
@@ -9,4 +9,12 @@ public interface IAuditLogService
     /// Logs an event asynchronously.
     /// </summary>
     Task LogAsync(LogActionType eventAction, string eventMessage, object? eventData = null, string? userEmail = null);
+
+    /// <summary>
+    /// Logs a validation error event asynchronously.
+    /// </summary>
+    /// <param name="eventMessage">The validation error message.</param>
+    /// <param name="dataRequest">Optional data related to the request that triggered the validation error.</param>
+    /// <param name="userEmail">Optional email of the user associated with the request.</param>
+    Task LogValidationErrorAsync(string eventMessage, object? dataRequest = null, string? userEmail = null);
 }
